aa372e0aedc7cf56da047a8e670d9fb4fead764f
DongWoo <osydooo@gmail.com>
2024-03-15 23:08:50 +0900

    [SCF-55] main layout - commerce main layout 구조 변경

--- apps/commerce/app/(main)/layout.tsx
+++ apps/commerce/app/(main)/layout.tsx
@@ -1,15 +1,14 @@
 import type { ReactNode } from "react";
 import { Layout } from "component/common/layout";
 
-const BookLayout = ({ children }: { children: ReactNode }) => {
+const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Layout.Header />
-      <Layout.Banner />
       <Layout.Main>{children}</Layout.Main>
       <Layout.Footer />
     </>
   );
 };
 
-export default BookLayout;
+export default MainLayout;
